6da146693882ceda11b55df47c80d57bbb486be2
Ben Major <ben.major88@gmail.com>
2022-12-24 13:22:45 +0000

    Add typing to Suggestion entity

--- src/Model/Suggestion.php
+++ src/Model/Suggestion.php
@@ -4,15 +4,23 @@ namespace BenMajor\GetAddress\Model;
 
 class Suggestion
 {
+    public const TYPE_ADDRESS = 'address';
+    public const TYPE_PLACE = 'place';
+
     private string $formatted;
     private string $endpoint;
     private string $id;
 
-    public function __construct(string $formatted, string $endpoint, string $id)
-    {
+    public function __construct(
+        string $formatted,
+        string $endpoint,
+        string $id,
+        string $type
+    ) {
         $this->endpoint = $endpoint;
         $this->formatted = $formatted;
         $this->id = $id;
+        $this->type = $type;
     }
 
     public function getId(): string
@@ -21,6 +29,26 @@ class Suggestion
     }
 
     public function getAddress(): string
+    {
+        return $this->formatted;
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function isAddress(): bool
+    {
+        return $this->type === self::TYPE_ADDRESS;
+    }
+
+    public function isPlace(): bool
+    {
+        return $this->type === self::TYPE_PLACE;
+    }
+
+    public function __toString(): string
     {
         return $this->formatted;
     }
