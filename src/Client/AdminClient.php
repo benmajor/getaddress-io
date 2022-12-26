18ca6a4fb1563e5d9492fb3430575df517857bf8
Ben Major <ben.major88@gmail.com>
2022-12-26 17:03:47 +0000

    Disable caching for admin client by default

--- src/Client/AdminClient.php
+++ src/Client/AdminClient.php
@@ -5,6 +5,7 @@ namespace BenMajor\GetAddress\Client;
 use GuzzleHttp\Client;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use DateTimeInterface;
+use InvalidArgumentException;
 
 class AdminClient extends AbstractClient implements ClientInterface
 {
@@ -13,18 +14,45 @@ class AdminClient extends AbstractClient implements ClientInterface
     public function __construct(string $apiKey)
     {
         $this->apiKey = $apiKey;
+        $this->cache = new FilesystemAdapter();
 
         $this->client = new Client([
             'base_uri' => sprintf('https://api.getAddress.io/v%d/', self::API_VERSION)
         ]);
 
-        $this->cache = new FilesystemAdapter();
+        $this->disableCaching();
     }
 
-    public function getUsage(DateTimeInterface $from, ?DateTimeInterface $to = null
-    )
+    public function getUsage(?DateTimeInterface $from = null, ?DateTimeInterface $to = null)
     {
+        $endpoint = '/usage';
 
+        if ($from !== null && $to !== null) {
+            if ($from > $to) {
+                throw new InvalidArgumentException('From date should be before to date.');
+            }
+
+            $endpoint .= sprintf(
+                '/from/%d/%d/%d/To/%d/%d/%d/',
+                $from->format('d'),
+                $from->format('m'),
+                $from->format('Y'),
+                $to->format('d'),
+                $to->format('m'),
+                $to->format('Y')
+            );
+        }
+        elseif ($from !== null) {
+            $endpoint .= sprintf(
+                '/%d/%d/%d/',
+                $from->format('d'),
+                $from->format('m'),
+                $from->format('Y')
+            );
+        }
+
+        print_r($this->getResponse($endpoint));
+        die();
     }
 
     public function addAddress()
